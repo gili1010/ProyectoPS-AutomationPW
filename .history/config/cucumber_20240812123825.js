62d67a9f6b490eb50876a05bf88964faf86e5154
gili1010 <lucianog_332@hotmail.com>
2024-08-12 23:21:05 -0300

    xray

---
+++ .history/config/cucumber_20240812123825.js
@@ -0,0 +1,44 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+const dotenv = require('dotenv');
+// Cargar variables de entorno del archivo .env
+dotenv.config();
+
+module.exports = {
+    
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+            timeout: 80000,
+        },
+        {
+            name: 'Microsoft Edge',
+            use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        },
+        {
+            name: 'Google Chrome',
+             use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+         },
+    ],
+    default: {
+        tags: process.env.npm_config_TAGS || "@nuevo",
+        formatOptions: {
+            snippetInterface: "async-await"
+        },
+        format: ['json:./cucumber-report.json'],
+        paths: [
+            "src/features/"
+        ],
+        require: [
+            "src/test/steps/*.ts",
+            "src/test/steps/*.ts",
+            "src/test/steps/apis/*.ts",
+            "src/hooks/hooks.ts"
+        ],
+        requireModule: [
+            "ts-node/register"
+        ]
+    }
+};
+
